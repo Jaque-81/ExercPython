c8e88bdcd557da489b2f6115d2e846f168a58c23
Jaque-81 <59448111+Jaque-81@users.noreply.github.com>
2021-11-15 10:13:14 -0300

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -8,3 +8,7 @@ class Cliente:
     def nome(self):#get não precisará ser usado por causa da ciração da propriedade
         print("Chamando @property nome ()")
         return self.__nome.title()#o método title garante que a primeira letra esteja em caixa alta
+    @nome.setter
+    def nome(self, nome):
+        print('Chamando setter nome()')
+            self.__nome = nome
