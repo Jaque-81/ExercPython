054743b07234b11f14bd2736ce9403adbee7f799
Jaque-81 <59448111+Jaque-81@users.noreply.github.com>
2021-11-15 10:20:54 -0300

    Update conta.py

--- conta.py
+++ conta.py
@@ -7,8 +7,9 @@ class Conta:#dentro da classe Conta será criado os objetos.
             self.__saldo = saldo
             self.__limite = limite
 
+
         def extrato(self):
-            print("Saldo de {} do titular {}".format(self.__saldo, self.__titular))
+            print("Saldo de {} do titular{}".format(self.__saldo, self.__titular))
 
         def deposita(self, valor):
             self.__saldo += valor
@@ -19,6 +20,18 @@ class Conta:#dentro da classe Conta será criado os objetos.
         def transfere(self, valor, destino):
             self.saca(valor)
             destino.deposita(valor)
+
+        def get_saldo(self):
+            return self._saldo
+
+        def get_titular(self):
+            return self._titular
+
+        def get_limite(self):
+            return self._limite
+
+        def set_limite(self, limite):
+            self._limite = limite
 
 
 """ Se eu importar a classe:
