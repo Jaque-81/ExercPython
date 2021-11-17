71618de25829d869432f61f03838863f9bd621db
Jaque-81 <59448111+Jaque-81@users.noreply.github.com>
2021-11-17 18:58:09 -0300

    Update conta.py

--- conta.py
+++ conta.py
@@ -1,53 +1,52 @@
-class Conta:#dentro da classe Conta será criado os objetos.
+class Conta:
 
-        def __init__(self, numero, titular, saldo, limite):#função construtora/ slf é a referência que sabe encontrar o obejto criado.
-            print("Construindo objeto ... {}".format(self))
-            self.__numero = numero
-            self.__titular = titular
-            self.__saldo = saldo
-            self.__limite = limite
+    def __init__(self, numero, titular, saldo, limite):
+        print("Construindo objeto ... {}".format(self))
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
+    def extrato(self):
+        print("Saldo de {} do titular {}".format(self.__saldo, self.__titular))
 
-        def extrato(self):
-            print("Saldo de {} do titular{}".format(self.__saldo, self.__titular))
+    def deposita(self, valor):
+        self.__saldo += valor
 
-        def deposita(self, valor):
-            self.__saldo += valor
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_a_sacar = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel_a_sacar
 
-        def saca(self, valor): #é necessário criar uma verificação para que não se saque qualquer valor.
+    def saca(self, valor):
+        if(self.__pode_sacar(valor)):
             self.__saldo -= valor
+        else:
+            print("O valor {} passou o limite".format(valor))
 
-        def transfere(self, valor, destino):
-            self.saca(valor)
-            destino.deposita(valor)
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
 
-          @property
-        def saldo(self):
-            return self.__saldo
+    @property
+    def saldo(self):
+        return self.__saldo
 
-        @property
-        def titular(self):
-            return self.__titular
+    @property
+    def get_titular(self):
+        return self.__titular
 
-        @property
-        def limite(self):
-            return self.__limite
+    @property
+    def limite(self):
+        return self.__limite
 
-        @limite.setter
-        def limite(self, limite):
-            self.__limite = limite
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
 
+    @staticmethod
+    def codigo_banco():
+        return "001"
 
-
-""" Se eu importar a classe:
-    Conta()
-Out[8]: <conta.Conta at 0x18391e82820>
-
-os números representam o lugar na memporia 
-    que foi armazenado o objeto. 
-    Se eu criar:
-        conta = Conta () /// objeto
-   Saída: conta
-Out[10]: <conta.Conta at 0x18391eaa490>
-'conta' é uma variável/referencial  que armazena o 
-objeto Conta()"""
+    @staticmethod
+    def codigos_bancos():
+        return {'BB':'001', 'Caixa':'104', 'Bradesco':'237'}
