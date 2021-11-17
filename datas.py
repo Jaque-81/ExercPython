b3af869d2953e358ad701865adc1c3845586da4c
Jaque-81 <59448111+Jaque-81@users.noreply.github.com>
2021-11-17 18:58:52 -0300

    Update datas.py

--- datas.py
+++ datas.py
@@ -1,8 +1,9 @@
 class Data:
+
     def __init__(self, dia, mes, ano):
         self.dia = dia
         self.mes = mes
         self.ano = ano
 
-    def mostraData(self):
-        print(f"{self.dia:2d}/{self.mes:2d}/{self.ano}")
+    def formatada(self):
+        print("{}/{}/{}".format(self.dia, self.mes,self.ano))
